7d36bf71b80cb9cdc56cf6d88ad8f9ac93d840c7
Evan Mora <emora1@uwyo.edu>
2017-12-01 18:57:11 -0700

    vote.cpp begin
    begin

--- vote.cpp
+++ vote.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "pMT.h"
+#include "bTREE.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
-	return 0;
+	
 }
